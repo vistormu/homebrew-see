d9956048b77c13ebbff5bb4b209449b79856f3bc
vistor <vistormu@gmail.com>
2025-07-03 20:46:52 +0200

    latest commit

--- Formula/see.rb
+++ Formula/see.rb
@@ -6,22 +6,22 @@ class See < Formula
   on_macos do
     on_arm   do
       url      "https://github.com/vistormu/see/releases/download/v#{version}/see_#{version}_darwin_arm64.tar.gz"
-      sha256   "sha256:2ba5186558332cdc7712c0de7a423656ce75c5e6b8753eb878cc7b334ea7c909"
+      sha256   "sha256:148da2f35f328d6a974de3e0ab42ca91e53f2fe71877221badcffe97e7e98f4d"
     end
     on_intel do
       url      "https://github.com/vistormu/see/releases/download/v#{version}/see_#{version}_darwin_amd64.tar.gz"
-      sha256   "sha256:fd007ddb80364954f13fcf703a127d2961054978820876475fca82a266da2095"
+      sha256   "sha256:154e9ce40d07892c520a34a9caba51dab122e61dd8d801b715de52e91ea7f468"
     end
   end
   
   on_linux do
     on_arm   do
       url      "https://github.com/vistormu/see/releases/download/v#{version}/see_#{version}_linux_arm64.tar.gz"
-      sha256   "sha256:22b30eaf99da124ff2053b18c3d316614db525357b7b18ae4cc4131af38c0602"
+      sha256   "sha256:b8cc892eaee8168ea1207df64aca73ee91c5d60fab3e22792cee92db803762e1"
     end
     on_intel do
       url      "https://github.com/vistormu/see/releases/download/v#{version}/see_#{version}_linux_amd64.tar.gz"
-      sha256   "sha256:4069a0c4864887baacc3222838c31e58154be2b711688f0734c64eb44c34312f"
+      sha256   "sha256:e0b0f59835349a76c215e1b79ac8a8814817d83815cca9cfa9e9a104daedd119"
     end
   end
   
